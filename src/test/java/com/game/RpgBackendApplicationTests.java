3bb6a631edcc1bc89d97925503e3f3fadbc8c38a
D0UGH5 <murilofneto1@gmail.com>
2025-10-17 10:54:44 -0300

    test: adicionar estrutura de testes - Criar RpgBackendApplicationTests - Configurar ambiente de testes Spring Boot - Preparar estrutura para testes unitários futuros

---
+++ src/test/java/com/game/RpgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.rpgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RpgBackendApplication.class)
+class RpgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
