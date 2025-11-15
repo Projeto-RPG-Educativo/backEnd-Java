dd169d86e9b494e1dd7226f761503c6bc20c222b
Murilo Furtado <murilofneto1@gmail.com>
2025-11-15 14:06:34 -0300

    adicionando url do frontend render no cors cofig

--- src/main/java/com/game/rpgbackend/config/SecurityConfig.java
+++ src/main/java/com/game/rpgbackend/config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173")); // Frontend URLs
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://frontend-client-jaal.onrender.com")); // Frontend URLs
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
